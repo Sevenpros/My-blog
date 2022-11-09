b524e931b460a85db2e321194ffe9661e472c233
Sevenpros <sethbisoso@gmail.com>
2022-11-09 14:25:01 +0100

    Add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_scope  :user do
+    get 'users/sign_out' => 'devise/sessions#destroy'
+    get 'users/sign_in' => 'devise/sessions#new'
+    root "devise/sessions#new"
 
+  end
   # Defines the root path route ("/")
-  # root "articles#index"
+  #  root "devise/sessions#new"
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index new show create] do
       resources :comments, only: %i[new create] 
